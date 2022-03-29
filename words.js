cf6ed02db50218e91f22820dab2f693f204c45f1
anjanai <39779866+anjanai@users.noreply.github.com>
2022-03-28 23:47:41 -0700

    Update words.js

--- words.js
+++ words.js
@@ -1,5 +1,4 @@
 //export const WORDS = `faltu useless`.split(/\s+/);
-export const WORDS = `
-fifty birds guitar jabalpur adobe 
+export const WORDS = `fifty birds guitar jabalpur adobe 
 ironing kachori noida bryant mittal
 kaddu`.split(/\s+/);
